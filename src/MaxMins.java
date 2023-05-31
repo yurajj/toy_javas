3823d71cf97cb270cd7b398e3fdb6132574809c0
yurajj <yul_j@naver.com>
2023-05-31 12:52:56 +0900

    yura commit

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class MaxMins {
-    
+ 
+    public static void main(String[] args) {
+        try {
+
+            Scanner num = new Scanner(System.in) ;
+
+            int a = num.nextInt() ;
+            int b = num.nextInt() ;
+
+            System.out.println("Min 값은 : " + Math.min(a, b) + "입니다.");
+
+            
+            num.close(); 
+
+        } catch(Exception e) {
+            // TODO ~
+        }
+        // return 0;
+    } 
 }
