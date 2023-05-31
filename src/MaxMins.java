0187b1cb208b33e7b0afd3d7c5d6d41ff64ce2b9
yurajj <yul_j@naver.com>
2023-05-31 13:15:02 +0900

    commit

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class MaxMins {
     public static void main(String[] args) {
         //상아 : max 
-        Scanner myObj = new Scanner (System.in);
+
+        try {
+        Scanner myObj = new Scanner (System.in); // 스캐너 사용
         // int first = myObj.nextInt();
         // int second = myObj.nextInt();
 //         System.out.print ("Max: "+ Math.max(first, second));
@@ -20,18 +22,18 @@ public class MaxMins {
 
             // Scanner num = new Scanner(System.in) ;
 
-            int a = myObj.nextInt() ;
-            int b = myObj.nextInt() ;
+            int a = myObj.nextInt() ; // 값 1 입력 받기
+            int b = myObj.nextInt() ; // 값 2 입력 받기
 
-            System.out.println ("Max : "+ Math.max(a, b));
-            System.out.println("Min 값은 : " + Math.min(a, b) + "입니다.");
+            System.out.println ("Max : "+ Math.max(a, b)); // 최대값 출력하기 
+            System.out.println("Min 값은 : " + Math.min(a, b) + "입니다."); // 최솟값 출력하기
 
             
-            myObj.close(); 
+            myObj.close();  // 스캐너 닫기
 
-        // } catch(Exception e) {
-        //     // TODO ~
-        // }
+        } catch(Exception e) {
+          // TODO ~
+        }
         // return 0;
     } 
 }
