207dd3e407dc0b5b7fb714c4f0d8fd4b38e1e164
yurajj <yul_j@naver.com>
2023-05-31 13:01:44 +0900

    commit

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -3,34 +3,36 @@ public class MaxMins {
     public static void main(String[] args) {
         //상아 : max 
         Scanner myObj = new Scanner (System.in);
-        int first = myObj.nextInt();
-        int second = myObj.nextInt();
-        System.out.print ("Max: "+ Math.max(first, second));
+        // int first = myObj.nextInt();
+        // int second = myObj.nextInt();
+//         System.out.print ("Max: "+ Math.max(first, second));
         
-        }
+//         }
         
-        // return 0 ;
+//         // return 0 ;
         
-    }
+//     }
 
-public class MaxMins {
+// public class MaxMins {
  
-    public static void main(String[] args) {
-        try {
+//     public static void main(String[] args) {
+//         try {
 
-            Scanner num = new Scanner(System.in) ;
+            // Scanner num = new Scanner(System.in) ;
 
-            int a = num.nextInt() ;
-            int b = num.nextInt() ;
+            int a = myObj.nextInt() ;
+            int b = myObj.nextInt() ;
 
+            System.out.println ("Max : "+ Math.max(a, b));
             System.out.println("Min 값은 : " + Math.min(a, b) + "입니다.");
 
             
-            num.close(); 
+            myObj.close(); 
 
-        } catch(Exception e) {
-            // TODO ~
-        }
+        // } catch(Exception e) {
+        //     // TODO ~
+        // }
         // return 0;
     } 
 }
+
