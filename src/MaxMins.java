906b36f25c256a51afaf120bbab5a1d05c68c585
SSSanga <ksa901125@gmail.com>
2023-05-31 12:54:44 +0900

    sanga_Max입니다!

--- src/MaxMins.java
+++ src/MaxMins.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
 public class MaxMins {
-    
-}
+    public static void main(String[] args) {
+        //상아 : max 
+        Scanner myObj = new Scanner (System.in);
+        int first = myObj.nextInt();
+        int second = myObj.nextInt();
+        System.out.print ("Max: "+ Math.max(first, second));
+        
+        }
+        
+        // return 0 ;
+        
+    }
